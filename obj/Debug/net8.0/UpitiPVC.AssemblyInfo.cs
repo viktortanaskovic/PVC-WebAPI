2b44825047455bf3dff4cbd7187df88270cacecc
Viktor Tanaskovic <viktor2001tanaskovic@gmail.com>
2026-02-19 10:18:05 +0100

    Izmenjena tabela Question ubaceno naslov i br telefona - Azure

--- obj/Debug/net8.0/UpitiPVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/UpitiPVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UpitiPVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+391c47b982fb6ff78b7029c4f04b55bc4753b0de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+384657762f299736ac414b116966ec070ec0df00")]
 [assembly: System.Reflection.AssemblyProductAttribute("UpitiPVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UpitiPVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
